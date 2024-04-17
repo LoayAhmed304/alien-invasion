4ea6ae42c44a90ef77bed917f35e3dabad20dafd
Loay Ahmed <loayahmed304@gmail.com>
2024-04-17 23:55:26 +0200

    Added ArrayADT

---
+++ Alien Invasion/DataStructures/ArrayADT.h
@@ -0,0 +1,17 @@
+#ifndef ARRAY_ADT_H
+#define ARRAY_ADT_H
+
+template <typename T>
+class ArrayADT
+{
+public:
+	virtual bool isEmpty() const = 0;
+	virtual bool insert(const T&) = 0;
+	virtual bool peek(T&, int) const = 0;
+	virtual bool remove(T&, int) = 0;
+	virtual int length() const = 0;
+	virtual void printAll() const = 0;
+	virtual ~ArrayADT(){ }
+};
+
+#endif

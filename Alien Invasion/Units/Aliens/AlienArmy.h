3c33eed78c069df74fde53ae351167b4d286bac7
Loay Ahmed <84445066+LoayAhmed304@users.noreply.github.com>
2024-04-25 18:06:57 +0200

    Update AlienArmy.h

--- Alien Invasion/Units/Aliens/AlienArmy.h
+++ Alien Invasion/Units/Aliens/AlienArmy.h
@@ -15,9 +15,6 @@ private:
     bool swap = true;
     bool swapPeek = true;
 
-    int shooterID;
-    string enemiesIDs;
-    string shooterType;
 public:
     bool addUnit(Units*);
     bool peekUnit(unitType, Units*&, int = 0);
@@ -27,8 +24,6 @@ public:
     void print();
     int length(unitType);
     bool fight(int m = 0);        // Peeks each earth unit and calls its attack
-    void setFight(Units*, LinkedQueue<Units*>);
-    void printFighting();
     ~AlienArmy();
 };
 

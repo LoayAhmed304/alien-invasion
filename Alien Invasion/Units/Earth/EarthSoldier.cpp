2ebfc2aacea1825f98dc5e2ee6317a699b6f6502
Omar Nabil <139366121+OmarNabil005@users.noreply.github.com>
2024-05-13 20:57:24 +0300

    ✨ Modified infected soldier's behaviour (#42)
    * print and healing mod
    
    * yellow
    
    * updates
    
    * added infected soldier's attack
    
    * added infected soldier's attack 1
    
    * changed spread infection's logic
    
    ---------
    
    Co-authored-by: KarimZakzouk <kokyfarid82@yahoo.com>

--- Alien Invasion/Units/Earth/EarthSoldier.cpp
+++ Alien Invasion/Units/Earth/EarthSoldier.cpp
@@ -9,50 +9,63 @@ EarthSoldier::EarthSoldier(int p, int h, int c, Game* g) : Units(earthSoldier, p
 bool EarthSoldier::attack()
 {
 	Units* enemy = nullptr;
-	Units* unit = this;
+	Units* self = this;
 	LinkedQueue<Units*> temp;
 	bool attacked = false;
-	for (int i = 0; i < this->getAttackCap(); ++i)
+	if (this->isInfected())
 	{
-		if (game->getUnit(alienSoldier, enemy))
-		{
-			if (!enemy->getTa())
-				enemy->setTa(game->getTimestep());
-			enemy->getAttacked(this->getPower() * this->getCurHealth() / 100);
-			temp.enqueue(enemy);
+		game->getUnit(earthSoldier, self);
 
-			if (!attacked)
+		for (int i = 0; i < this->getAttackCap(); ++i)
+		{
+			if (game->getUnit(earthSoldier, enemy))
 			{
-				game->toLog(unit, enemy);
-				attacked = true;
+				if (!enemy->getTa())
+					enemy->setTa(game->getTimestep());
+				enemy->getAttacked(this->getPower() * this->getCurHealth() / 100);
+				temp.enqueue(enemy);
+
+				if (!attacked)
+				{
+					game->toLog(self, enemy);
+					attacked = true;
+				}
+				else
+					game->toLog(enemy);
 			}
-			else
+		}
+		game->addUnit(self);
+	}
+	else
+	{
+		for (int i = 0; i < this->getAttackCap(); ++i)
+		{
+			if (game->getUnit(alienSoldier, enemy))
 			{
-				game->toLog(enemy);
+				if (!enemy->getTa())
+					enemy->setTa(game->getTimestep());
+				enemy->getAttacked(this->getPower() * this->getCurHealth() / 100);
+				temp.enqueue(enemy);
+
+				if (!attacked)
+				{
+					game->toLog(self, enemy);
+					attacked = true;
+				}
+				else
+				{
+					game->toLog(enemy);
+				}
 			}
 		}
 	}
 	if (attacked)
 		game->toLog();
-	if (game->canSpread() && this->isInfected())
-	{
-		Units* toInfect = nullptr;
-		if (game->getRandomES(toInfect) && (toInfect != this))
-		{
-			if (toInfect->getInfected())
-			{
-				game->toLog(unit, toInfect);
-				game->toLog();
-			}
-		}
-	}
 
 	while (temp.dequeue(enemy))
 	{
 		if (enemy->isDead())
-		{
 			game->kill(enemy);
-		}
 		else
 			game->addUnit(enemy);
 	}
